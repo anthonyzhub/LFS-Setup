406a026149284a4191ec1b0d86b2018ac25a4c77
Anthony Z <zamora.anthony1234@gmail.com>
2022-02-22 16:54:33 -0800

    Bug fixed

--- setup.sh
+++ setup.sh
@@ -6,6 +6,10 @@ sudo apt update -y && sudo apt upgrade -y
 # Install new packages
 sudo apt install -y build-essential bison bzip2 python3 texinfo gawk
 
+# Update sh symbolic link from dash to bash
+sudo rm /usr/bin/sh
+sudo ln -s /usr/bin/bash /usr/bin/sh
+
 # Create a directory for USB to mount on
 sudo mkdir /mnt/lfs
 sudo mkdir /mnt/lfs/boot
@@ -16,12 +20,12 @@ sudo mkdir /mnt/lfs/sources
 sudo chmod a+wt /mnt/lfs/sources
 
 # Create a lfs variable
-echo "export lfs=/mnt/lfs" >> /home/anthony/.bashrc
+echo "export LFS=/mnt/lfs" >> /home/anthony/.bashrc
 source /home/anthony/.bashrc
 
 # Download wget-list then download all packages listed inside file
 sudo wget https://mirror.jaleco.com/lfs/pub/lfs/lfs-packages/11.0/wget-list
-sudo wget --input-file=wget-list --continue --directory-prefix=$lfs/sources
+sudo wget --input-file=wget-list --continue --directory-prefix=/mnt/lfs/sources
 
 # Print message
 echo
